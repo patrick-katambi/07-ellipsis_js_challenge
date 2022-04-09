65722dbcf62b5fce8f4ea4755b18334e3bd7584e
patrick-katambi <katambipatrick@gmail.com>
2022-04-09 17:20:09 +0100

    page navigation setup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/index'
+import Series from './pages/series/index'
+import Movies from './pages/movies/index'
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="series" element={<Series />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="*" element={<p>404 error</p>} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route, Link, NavLink } from "react-router-dom";
 
 function NavigationBar() {
   return (
-    <div className=' bg-[red] '>NavigationBar</div>
-  )
+    <div className=" w-full h-[80px] px-[10%] flex items-center justify-between bg-[beige] ">
+      <Logo />
+      <MenuLinksSection />
+      <div>Buttons</div>
+    </div>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
+
+function Logo() {
+  const logoNameFirstPart = "Demo";
+  const logoNameSecondPart = "Streaming";
+  return (
+    <div>
+      <p className=" font-bold ">
+        {logoNameFirstPart}
+        <span className=" font-thin ">{logoNameSecondPart}</span>
+      </p>
+    </div>
+  );
+}
+
+function MenuLinksSection(props) {
+  const navItems = [
+    { path: "/home", label: "Home" },
+    { path: "/movies", label: "Movies" },
+    { path: "/series", label: "Series" },
+  ];
+  return (
+    <nav className=" flex items-center ">
+      {navItems.map((navItem, index) => {
+        return <NavItem path={navItem.path} label={navItem.label} />;
+      })}
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  return (
+    <ul className=" mx-5 font-bold tracking-widest ">
+      <NavLink
+        to={props.path}
+        style={({ isActive }) => {
+          return {
+              padding: '20px',
+            fontSize: isActive ? "20px" : "15px",
+            color: isActive ? "black" : "grey",
+            transition: '0.3s'
+          };
+        }}
+      >
+        {props.label}
+      </NavLink>
+    </ul>
+  );
+}
